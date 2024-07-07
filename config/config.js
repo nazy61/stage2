c90249ec31523fcd60ca3062e090ef445da512bc
Ogwo Chinaza <ogwo.chinaza@yahoo.com>
2024-07-07 20:07:09 +0100

    deployment to vercel done

---
+++ config/config.js
@@ -0,0 +1,29 @@
+require("dotenv").config(); // this is important!
+const pg = require("pg");
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    dialectModule: pg,
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    dialectModule: pg,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    dialectModule: pg,
+  },
+};
